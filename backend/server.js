cd493faa39795eb50e570ead4f9cfb8578681673
shivam <shivam.singhal.211@gmail.com>
2025-02-23 23:01:17 +0530

    Final Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 //package import
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"; //cookie parser
@@ -11,18 +12,24 @@ import connectToDB  from "./DB/ConnectToDB.js";
 
 import {app,server} from './socket/socket.js'
 
+const __dirname = path.resolve();
 dotenv.config();
 
 const PORT = process.env.PORT || 6000;
 
+
 //middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
 //routes
 app.use("/api/auth", authRoutes);
 app.use('/api/message', messageRoutes);
 app.use('/api/users', userRoutes);
+app.use("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+});
 
 
 server.listen(PORT, async(req, res) => {
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,43 +1,38 @@
-import express from 'express';
-import {Server} from 'socket.io';
-import http from 'http';
+import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 
 const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors: {
-        origin: ['http://localhost:4000'],
-        methods: ["GET","POST"]
-    }
-})
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST"],
+  },
+});
 
-const usersocketMap = {} //{user_id:socket_id}
+const usersocketMap = {}; //{user_id:socket_id}
 
-export const getReceiverSocketID = (recevierId)=>{
-    return usersocketMap[recevierId];
-}
+export const getReceiverSocketID = (recevierId) => {
+  return usersocketMap[recevierId];
+};
 
-io.on("connection",(socket)=>{
-    console.log("a new user is connected",socket.id);
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
 
-const userId = socket.handshake.query.userId;
-
-if(userId!=="undefined")
-{
+  if (userId !== "undefined") {
     usersocketMap[userId] = socket.id;
-}
+  }
 
-io.emit("getOnlineUsers",Object.keys(usersocketMap));
+  io.emit("getOnlineUsers", Object.keys(usersocketMap));
 
+  //socket.on is used to listen the events on both side client and server
+  socket.on("disconnect", () => {
+    delete usersocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(usersocketMap));
+  });
+});
 
-//socket.on is used to listen the events on both side client and server
-    socket.on("disconnect",()=>{
-        console.log("user disconnected",socket.id);
-        delete usersocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(usersocketMap));
-    })
-})
-
-export {app,server,io};
+export { app, server, io };
