42146ad59cc438c0d85783bff8cc6f08f369b258
shivam <shivam.singhal.211@gmail.com>
2025-02-24 22:29:01 +0530

    Socket URL update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -19,7 +19,7 @@ export const SocketContextProvider = ({children})=>{
     useEffect(()=>{
         if(authUser)
         {
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chat-app-o0b0.onrender.com",{
                 query:{
                     userId: authUser._id,
                 }
